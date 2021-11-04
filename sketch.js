78947433f18e25ccde8b5c1c4867d23e60c132ea
EKTG <93701727+EKTG@users.noreply.github.com>
2021-11-04 15:13:03 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(150,40,10,10);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown("d")){
+    box.x= box.x+2;
+  }
+  if (keyDown("a")){
+    box.x=box.x-2
+  }
+  if (keyDown("w")){
+    box.y=box.y-2
+  }
+  if (keyDown("s")){
+    box.y=box.y+2
+  }
+drawSprites();
+}
+
+
+
+
